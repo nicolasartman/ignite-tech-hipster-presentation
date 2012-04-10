4430ce0e6d16c130997e5424fa00927f001b1bc4
Nicolas Artman <nicolasartman@gmail.com>
2012-04-10 00:09:08 -0700

    initial commit

---
+++ ignite/ignite.js
@@ -0,0 +1,33 @@
+/*
+  Ignite Specific Additions
+*/
+(function($, deck, undefined) {
+	var $d = $(document)
+	
+	var autoAdvancing = false
+	
+	$d.bind('deck.change', function (event, from, to) {
+	  $subs = $.deck('getSlide', to).find('.sub')
+	  $.each($subs, function(index, sub) {
+	    if ($(sub).attr('delay')) {
+	      $(sub).hide()
+	      // show after the specified time
+	      setTimeout(function () {
+	        $(sub).show()
+	      }, $(sub).attr('delay') * 1000)
+	    }
+	  })
+	  
+	  // Enable autoadvance once we reach slide 1
+    if (!autoAdvancing && to === 1) {
+      console.log("auto-advancing")
+      autoAdvancing = true
+      // Autoadvance every 15 seconds
+      var autoAdvance = setInterval(function () {
+          $.deck('next')
+      }, 15000)
+
+      clearInterval(autoAdvance)
+    }
+	})
+})(jQuery, 'deck');
